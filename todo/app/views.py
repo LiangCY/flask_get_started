43354a750d693db9fe8405a93bbc2b75b315a1e6
LiangCY <liangcynju@qq.com>
2015-12-19 17:30:20 +0800

    todo

--- todo/app/views.py
+++ todo/app/views.py
@@ -1,7 +1,12 @@
 from app import app
 from flask import render_template
+from models import Todo
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print('123')
+    todos = Todo.objects.all()
+
+    print(todos)
+    return render_template('index.html', todos=todos)
