07e18ba1750ca31e2cc78ed02b631d5be7c3775b
LiangCY <liangcynju@qq.com>
2015-12-20 19:33:23 +0800

    todo: add and query

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,9 +1,13 @@
 import datetime
 
 from app import db
+from flask.ext.mongoengine.wtf import model_form
 
 
 class Todo(db.Document):
-    content = db.StringField()
+    content = db.StringField(required=True, max_length=20)
     time = db.DateTimeField(default=datetime.datetime.now())
     status = db.IntField(default=0)
+
+
+TodoForm = model_form(Todo)
--- todo/config.py
+++ todo/config.py
@@ -1,3 +1,5 @@
+SECRET_KEY = "TODO"
+
 MONGODB_SETTINGS = {
     'db': 'todo',
     'username': 'lcy',
